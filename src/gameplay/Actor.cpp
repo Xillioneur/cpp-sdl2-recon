470b7e1f36f096752374172f92f3cb9982fa2918
Xillioneur <241650870+Xillioneur@users.noreply.github.com>
2026-04-24 08:47:28 -0400

    Code drawWeapon method and implement it into the game.

--- src/gameplay/Actor.cpp
+++ src/gameplay/Actor.cpp
@@ -1,5 +1,25 @@
 #include "Actor.hpp"
 
+namespace Graphics {
+    void drawWeapon(SDL_Renderer* ren, Vec2 center, float lookAngle, int length, int width, SDL_Color col, float handOffset) {
+        float handAngle = lookAngle + 1.5708f;
+        Vec2 handPos = center + Vec2(std::cos(handAngle) * handOffset, std::sin(handAngle) * handOffset);
+        float c = std::cos(lookAngle);
+        float s = std::sin(lookAngle);
+        int ex = (int)(handPos.x + c * length);
+        int ey = (int)(handPos.y + s * length);
+        SDL_SetRenderDrawColor(ren, col.r, col.g, col.b, 255);
+        for (int i = -width/2; i <= width/2; ++i) {
+            float ox = -s * i, oy = c * i;
+            SDL_RenderDrawLine(ren, (int)(handPos.x + ox), (int)(handPos.y + oy), (int)(ex + ox), (int)(ey + oy));
+        }
+        SDL_SetRenderDrawBlendMode(ren, SDL_BLENDMODE_BLEND);
+        SDL_SetRenderDrawColor(ren, 255, 50, 50, 50);
+        SDL_RenderDrawLine(ren, (int)handPos.x, (int)handPos.y, (int)(handPos.x + c * 150.0f), (int)(handPos.y + s * 150.0f));
+        SDL_SetRenderDrawBlendMode(ren, SDL_BLENDMODE_NONE);
+    }
+}
+
 Player::Player(Vec2 p) : Entity(p, 24, 24, EntityType::PLAYER) {}
 void Player::update(float dt, const std::vector<std::vector<Tile>>& map) {
     Entity::update(dt, map);
@@ -16,6 +36,6 @@ void Player::render(SDL_Renderer* ren, const Vec2& cam) {
     SDL_Rect visor = {r.x + 6, r.y + 2, 12, 4};
     SDL_SetRenderDrawColor(ren, 100, 255, 255, 255); 
     SDL_RenderFillRect(ren, &visor);
-    // TODO: Draw Weapon
+    Graphics::drawWeapon(ren, { (float)r.x + 12, (float)r.y + 12 }, lookAngle, 22, 6, {100, 110, 120, 255}, 0.0f);
     // TODO: Dash Timer
 }
--- src/gameplay/Actor.hpp
+++ src/gameplay/Actor.hpp
@@ -3,6 +3,10 @@
 
 #include "../engine/Entity.hpp"
 
+namespace Graphics {
+    void drawWeapon(SDL_Renderer* ren, Vec2 center, float lookAngle, int length, int width, SDL_Color col, float handOffset = 0.0f);
+}
+
 class Player : public Entity {
 public:
     Player(Vec2 p);
